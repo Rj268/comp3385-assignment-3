7cc480a337fe43c3c18a3bba0a3c797bf1d85f3d
Ronnie Samuel <samuelronnie55@gmail.com>
2025-03-09 22:37:19 -0400

    Added login.blade.php

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,15 @@
+<form action="{{ url('/login') }}" method="POST">
+    @csrf
+    <label>Email:</label>
+    <input type="email" name="email" required>
+    
+    <label>Password:</label>
+    <input type="password" name="password" required>
+    
+    <button type="submit">Login</button>
+</form>
+
+<!-- Display errors -->
+@if(session('error'))
+    <div>{{ session('error') }}</div>
+@endif
