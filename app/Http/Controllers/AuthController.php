d6b1e2c6129d878956371a807e288126364736cf
Ronnie Samuel <samuelronnie55@gmail.com>
2025-03-09 22:36:18 -0400

    Added AuthController.php

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function create()
+    {
+        return view('auth.login');
+    }
+
+    public function store(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect('/dashboard')->with('message', 'Login successful');
+
+        }
+        return back()->with('error', 'Invalid credentials. Check the email address and password entered.');
+    }
+}
